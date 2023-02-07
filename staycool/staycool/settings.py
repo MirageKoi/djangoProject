75d0f6ba69726d656bc3e0417ff895b37f79b1bd
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-02-07 18:39:00 +0200

    finished with view and basic templates for task_list

--- staycool/staycool/settings.py
+++ staycool/staycool/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "task.apps.TaskConfig",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'staycool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'staycool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test003',
+        'USER': 'postgres',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- staycool/task/views.py
+++ staycool/task/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+tasks = [
+    {"id": 1, "title": "Title 1", "description": "Some long task description...", "created": "2022-01-24 12:00",
+     "updated": "2022-01-25 12:00", "completed": False},
+    {"id": 2, "title": "Title 2", "description": "Some long task description...", "created": "2022-01-24 12:00",
+     "updated": "2022-01-25 12:00", "completed": True},
+    {"id": 3, "title": "Title 3", "description": "Some long task description...", "created": "2022-01-24 12:00",
+     "updated": "2022-01-25 12:00", "completed": False},
+]
+
+
+def homepage(request: HttpRequest):
+    return render(request, "base.html")
+
+
+def task_list_view(request: HttpRequest):
+    return render(request, "index.html", {"tasks": tasks})
+
+
+def task_detailed(request, number: int):
+    for obj in tasks:
+        if obj["id"] == number:
+            return render(request, "single.html", {"object": obj})
+
+
+def form_view(request):
+    return render(request, 'form.html')
