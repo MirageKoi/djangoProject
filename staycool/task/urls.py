c337c1c46689d3f916e991f077c76e8be92d70bb
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-02-13 14:58:27 +0200

    added and adopted templates from bootstrap

---
+++ staycool/task/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path('form/', views.form_view, name='form-view'),
+    path("task/", views.task_list_view, name="task_list_view"),
+    path("task/<int:number>/", views.task_detailed, name="task_detailed")
+]
