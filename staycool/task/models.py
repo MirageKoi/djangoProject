251333b2840b149efb0eb1a83cf0bbe8f82a0fcd
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-02-14 09:49:33 +0200

    code refactoring

--- staycool/task/models.py
+++ staycool/task/models.py
@@ -2,9 +2,5 @@ from django.db import models
 
 
 class Article(models.Model):
-    name = models.CharField( max_length=50)
-    title = models.CharField( max_length=50)
-
-
-class Hello(models.Model):
-    name = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
